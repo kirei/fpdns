39ae7fca8c1e24fdf8812887de0f47abcb4e599e
Serign Jobe <serignjobe@gmail.com>
2011-06-29 22:53:56 -0400

    Outputs a single tree instead of multiple ones. Perl output functionality method added

---
+++ utils/FingerprintTreeGenerator/Query.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author sjobe
+ *
+ * Is NOT meant to be an exact model of a DNS query,
+ * please don't have my head for it. Think of it as
+ * a way to avoid using multiple arrays with corresponding
+ * indexes to keep track of some of the details of a query. 
+ *
+ */
+public class Query {
+  String header;
+  String nameClassType;
+}
